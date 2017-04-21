b2f48a586029d82cac707d1b478ab7251a8a634e
Koutaroo <sss5555656sksd555656ktrktr@gmail.com>
2017-04-21 23:58:06 +0900

    A03

---
+++ src/ofApp.cpp
@@ -0,0 +1,127 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+}
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetLineWidth(10);
+    ofBackground(255);
+    
+    x = 190;
+    
+    ofSetColor(255, 228, 0);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    ofSetColor(0, 123, 120);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    
+    ofSetColor(113, 115, 109);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    
+    ofSetColor(153, 156, 164);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    
+    ofSetColor(178, 154, 122);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    
+    ofSetColor(200);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    
+    ofSetColor(0, 123, 120);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    
+    ofSetColor(113, 115, 109);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    
+    ofSetColor(153, 156, 164);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    
+    ofSetColor(178, 154, 122);
+    ofDrawLine(x,200,x,550);
+    x +=70;
+    
+    
+    
+    
+    
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
